782d14f00353b27090908b855dd59fc1f7a64c07
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2024-09-07 21:11:37 +0530

    [AS-02]: Form submission

--- Frontend/src/Components/Container.jsx
+++ Frontend/src/Components/Container.jsx
@@ -4,37 +4,88 @@ import styles from "./container.module.css";
 
 const Container = () => {
   const [tab, setTab] = useState(true);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  function setSwitchTab() {
+  function handleTabSwitch() {
     setTab(!tab);
   }
+
+  const handleInputChange = (e) => {
+    // dynamically update the field based on the input name
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const handleSubmission = (e) => {
+    e.prevenDefault();
+    console.log("Form submitted", data);
+  };
 
   return (
     <div className={styles.box}>
       <div className={styles.tabs}>
-        <span className={tab ? "" : styles.tab} onClick={setSwitchTab}>
+        <span className={tab ? "" : styles.tab} onClick={handleTabSwitch}>
           Log In
         </span>
-        <span className={tab ? styles.tab : ""} onClick={setSwitchTab}>
+        <span className={tab ? styles.tab : ""} onClick={handleTabSwitch}>
           Sign Up
         </span>
       </div>
       <div className={styles.form}>
-        {tab ? (
-          <>
-            <input placeholder="Enter your Name" />
-            <input placeholder="Enter your Email" />
-            <input placeholder="Enter your password" />
-          </>
-        ) : (
-          <>
-            <input placeholder="Enter your Email" />
-            <input placeholder="Enter your password" />
-          </>
-        )}
-        <div className={styles.button}>
-          <button type="submit">Submit</button>
-        </div>
+        <form onSubmit={handleSubmission}>
+          {tab ? (
+            <>
+              <input
+                placeholder="Enter your Name"
+                name="name"
+                value={data.name}
+                onChange={handleInputChange}
+                required
+              />
+              <input
+                placeholder="Enter your Email"
+                name="email"
+                value={data.email}
+                onChange={handleInputChange}
+                required
+              />
+              <input
+                placeholder="Enter your password"
+                name="password"
+                value={data.password}
+                onChange={handleInputChange}
+                required
+              />
+            </>
+          ) : (
+            <>
+              <input
+                placeholder="Enter your Email"
+                name="email"
+                value={data.email}
+                onChange={handleInputChange}
+                required
+              />
+              <input
+                placeholder="Enter your password"
+                name="password"
+                value={data.password}
+                onChange={handleInputChange}
+                required
+              />
+            </>
+          )}
+          <div className={styles.button}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     </div>
   );
