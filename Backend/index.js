a8309ca9c3451f5be19c4026b87b493a639a8e9e
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2024-09-14 21:03:36 +0530

    JWT for user registration.

--- Backend/index.js
+++ Backend/index.js
@@ -22,30 +22,6 @@ app.listen(PORT, () => {
     console.log("server is running on 3000");
 });
 
-app.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
-
-    // hash the password
-    try {
-        const saltRounds = 10;
-        // bcrypt.hash() is a async function that hashes the password.
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const query = `
-        INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *
-        `; // parameterized queries, RETURNING * clause makes sure inserted user data is returned.
-        const values = [name, email, hashedPassword];
-        const result = await pool.query(query, values);
-
-        res.status(201).json({
-            message: "User registered successfully",
-            user: result.rows[0], // return user data without id
-        });
-    } catch (error) {
-        console.error("Error registering user:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-});
-
 // Middleware to verify JWT 
 const authenticateJwtToken = (req, res, next) => {
     //
@@ -65,6 +41,37 @@ const authenticateJwtToken = (req, res, next) => {
     }
 }
 
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // hash the password
+    try {
+        const saltRounds = 10;
+        // bcrypt.hash() is a async function that hashes the password.
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const query = `
+        INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *
+        `; // parameterized queries, RETURNING * clause makes sure inserted user data is returned.
+        const values = [name, email, hashedPassword];
+        const result = await pool.query(query, values);
+        const user = result.rows[0];
+
+        // create a JWT token 
+        const token = jwt.sign({ id: user.id, email: user.email }, 'myNextJwtSecret', {
+            expiresIn: '1h'
+        })
+       
+        res.status(201).json({
+            token,
+            message: "User registered successfully",
+            user: result.rows[0], // return user data without id
+        });
+    
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
 
 // protected route 
 router.get('dashboard', authenticateJwtToken, (req, res) => {
